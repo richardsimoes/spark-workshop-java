6f1e4c9b958248a857cb37acfec2d0ae2d27abe1
Richard Simoes <richard.simoes@gmail.com>
2016-02-20 23:34:33 -0600

    Update

---
+++ src/main/Java/com/datasignal/spark/Main.java
@@ -0,0 +1,12 @@
+package com.datasignal.spark;
+
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * Created by rsimoes on 2/20/16.
+ */
+public class Main {
+    public static void main (String[] args) {
+        JavaSparkContext sc = new JavaSparkContext();
+    }
+}
